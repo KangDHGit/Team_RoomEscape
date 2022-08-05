d115094c7a2624e46caba99afc060c02ef6107b1
KangDHGit <eogks0323@gmail.com>
2022-08-05 15:36:30 +0900

    일부스크립트 수정

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -9,16 +9,19 @@ namespace RoomEscape
         public static CameraManager CamMgr;
         private GameObject _objMCam_Now; // 현재 내가 있는방의 메인카메라
         private GameObject _objZCam_Now; // 현재 내가 있는방의 활성화된 줌카메라
+        public List<Camera> _MCamList;
 
         private void Awake()
         {
             CamMgr = this;
+            _MCamList = new List<Camera>(GetComponentsInChildren<Camera>());
         }
 
         public void SetMCam(GameObject objMCam)
         {
             _objMCam_Now = objMCam;
         }
+
         public void SetZCam(GameObject objZCam)
         {
             _objZCam_Now = objZCam;
@@ -26,8 +29,6 @@ namespace RoomEscape
 
         public void ChangeView(bool isMainView) // true : 메인카메라 켜짐, false : 줌카메라 켜짐
         {
-            if(_objMCam_Now != null)
-                _objMCam_Now.SetActive(isMainView);
             if(_objZCam_Now != null)
                 _objZCam_Now.SetActive(!isMainView);
         }
--- Assets/Script/RoomObj.cs
+++ Assets/Script/RoomObj.cs
@@ -17,14 +17,14 @@ namespace RoomEscape
     {
         
         Room _room = Room.CENTER; // 이 오브젝트가 속한 방
-        GameObject _objMCam; // 방 메인(Main) 카메라
-        GameObject _objZCam; // 오브젝트 클릭시 활성화할 줌(Zoom) 카메라
+        public GameObject _objMCam; // 방 메인(Main) 카메라
+        public GameObject _objZCam; // 오브젝트 클릭시 활성화할 줌(Zoom) 카메라
 
 
         void Start()
         {
-            _objMCam = CameraManager.CamMgr.transform.Find($"{_room}/Cam_{_room}").gameObject;
-            _objZCam = CameraManager.CamMgr.transform.Find($"{_room}/Cam_{gameObject.name}").gameObject;
+            _objMCam = CameraManager.CamMgr.transform.Find($"Cam_{_room}").gameObject;
+            _objZCam = CameraManager.CamMgr.transform.Find($"Cam_{_room}/Cam_{gameObject.name}").gameObject;
             if (_objZCam != null)
                 _objZCam.SetActive(false);
         }
