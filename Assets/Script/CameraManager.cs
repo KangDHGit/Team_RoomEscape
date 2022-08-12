c92e381822e2d71a489c76766fcf178a5c18f7fb
KangDHGit <eogks0323@gmail.com>
2022-08-12 15:41:36 +0900

    기초코드 구현중

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -6,36 +6,19 @@ namespace RoomEscape
 {
     public class CameraManager : MonoBehaviour
     {
-        public static CameraManager CamMgr;
+        public static CameraManager CamMgr; // 싱글턴
+
         private Room _room_Now; // 내가있는방
         private GameObject _objZCam_Now; // 현재 내가있는방의 활성화된 줌카메라
-        public List<Camera> _MCamList;
-
-
+        private GameObject _objMCam_Now; // 현재 내가있는방의 메인카메라
+        public List<GameObject> _objMCam_List;
 
         private void Awake()
         {
             CamMgr = this;
-            _MCamList = new List<Camera>(GetComponentsInChildren<Camera>());
-        }
-
-
-        public void SetCam(RoomObj roomObj)
-        {
-            _objZCam_Now = roomObj._objZCam; // 줌카메라 셋팅
-            if (_room_Now == roomObj._room) // 같은 방일경우 탈출
-                return;
-            else
-            {
-                _room_Now = roomObj._room; // 내가있는방 변경
-            }
-        }
-        public void ChangeView(bool isMainView) // true : 메인카메라 켜짐, false : 줌카메라 켜짐
-        {
-            if (_objZCam_Now != null)
-            {
-                _objZCam_Now.SetActive(!isMainView);
-            }
+            _room_Now = Room.SPRING;
+            _objMCam_List = new List<GameObject>(GameObject.FindGameObjectsWithTag("MainCamera"));
+            _objMCam_List[(int)_room_Now].SetActive(true);
         }
     }
 }
--- Assets/Script/RoomObj.cs
+++ Assets/Script/RoomObj.cs
@@ -7,33 +7,33 @@ namespace RoomEscape
 {
     public enum Room
     {
-        CENTER,
-        ROOM1,
-        ROOM2,
-        ROOM3,
-        ROOM4,
+        None = -1,
+        SPRING,
+        SUMMER,
+        FALL,
+        WINTER
     }
     public class RoomObj : MonoBehaviour
     {
         
-        public Room _room = Room.CENTER; // 이 오브젝트가 속한 방
-        //public GameObject _objMCam; // 방 메인(Main) 카메라
+        public Room _room; // 이 오브젝트가 속한 방
         public GameObject _objZCam; // 오브젝트 클릭시 활성화할 줌(Zoom) 카메라
 
 
         void Start()
         {
-            //_objMCam = CameraManager.CamMgr.transform.Find($"Cam_{_room}").gameObject;
-            _objZCam = CameraManager.CamMgr.transform.Find($"Cam_{_room}/Cam_{gameObject.name}").gameObject;
-            if (_objZCam != null)
-                _objZCam.SetActive(false);
+            if (_room != Room.None)
+            {
+                _objZCam = CameraManager.CamMgr.transform.Find($"{_room}/Cam_{gameObject.name}").gameObject;
+                if (_objZCam != null)
+                    _objZCam.SetActive(false);
+            }
         }
 
         private void OnMouseDown()
         {
-            //CameraManager.CamMgr.SetMCam(_objMCam);
-            CameraManager.CamMgr.SetCam(this);
-            CameraManager.CamMgr.ChangeView(false);
+            _objZCam.SetActive(true);
+            GetComponent<Collider>().enabled = false;
         }
     }
 }
