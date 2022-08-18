2d25a94d6c796be145d9674047e6f05abcf69073
KangDHGit <eogks0323@gmail.com>
2022-08-18 15:53:41 +0900

    TestUI생성

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -17,10 +17,16 @@ namespace RoomEscape
             _uiMgr = this;
             _gRayCaster = GetComponent<GraphicRaycaster>();
         }
+        private void Update()
+        {
+            //if(Input.GetMouseButtonDown(0))
+            //    CheckClickUI();
+        }
 
         public bool CheckClickUI()
         {
-            PointerEventData data = new PointerEventData(EventSystem.current); // 
+            PointerEventData data = new PointerEventData(EventSystem.current);
+            // EventSystem.current : 씬에있는 현재 이벤트 시스템
             data.position = Input.mousePosition;
 
             List<RaycastResult> results = new List<RaycastResult>();
@@ -29,6 +35,10 @@ namespace RoomEscape
 
             if (results.Count > 0)
             {
+                foreach (var item in results)
+                {
+                    Debug.Log(item.gameObject.name);
+                }
                 return true;
             }
             else
