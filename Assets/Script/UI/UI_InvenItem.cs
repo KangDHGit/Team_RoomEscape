97b909552f90dc86b89103b5f88ee1369a912e09
KangDHGit <eogks0323@gmail.com>
2022-08-18 23:19:46 +0900

    UI드래그 스크립트 생성(구현x)

---
+++ Assets/Script/UI/UI_InvenItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_InvenItem : MonoBehaviour
+{
+    
+    void DragItem()
+    {
+       
+    }
+}
