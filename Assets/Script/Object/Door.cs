779488ffeed8b99d3ccf926fc0bf59b61e49bfea
KangDHGit <eogks0323@gmail.com>
2022-08-19 15:03:46 +0900

    아이템 드래그 앤 드롭시 상호작용 기초 구현

--- Assets/Script/Object/Door.cs
+++ Assets/Script/Object/Door.cs
@@ -8,17 +8,34 @@ namespace RoomEscape
     {
         // 문이 연결되있는 방
         public Room _roomOut;
-        // 열려있는지 여부
-        public bool _isOpen;
+
+        public string _requireItemName;
+
+        // 조건만족 여부
+        bool _satisfaction = false;
 
         private void OnMouseDown()
         {
-            if(_isOpen && !CameraManager.CamMgr._isZoom)
+            if(_satisfaction && !CameraManager.CamMgr._isZoom)
             {
                 CameraManager.CamMgr.SetMCam(_roomOut);
             }
             else
                 Debug.Log("Door is Close or Zoom Activate");
+        }
+
+        public bool CheckItem(string itemName)
+        {
+            if(_requireItemName == itemName)
+            {
+                _satisfaction = true;
+                transform.Rotate(new Vector3(0, -45, 0));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
