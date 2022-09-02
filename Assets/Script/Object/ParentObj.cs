841b06ec1c420c67a828bcb62ed511dcd3d99ee4
KangDHGit <eogks0323@gmail.com>
2022-09-02 12:46:06 +0900

    부모obj 분리작업중

---
+++ Assets/Script/Object/ParentObj.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoomEscape
+{
+    public class ParentObj : RoomObj
+    {
+        public List<RoomObj> _list_ChildRoomObj; // 오브젝트의 자식 오브젝트
+
+        protected override void Start()
+        {
+            base.Start();
+            _list_ChildRoomObj = new List<RoomObj>(GetComponentsInDirectChild<RoomObj>());
+            ListSetCol(_list_ChildRoomObj, false);
+        }
+
+        protected override void OnMouseDown()
+        {
+            if (!UIManager.I.CheckClickUI() && !CameraManager.I._isZoom)
+            {
+                if (_objZCam != null)
+                {
+                    ListSetCol(_list_ChildRoomObj, true);
+                }
+            }
+            base.OnMouseDown();
+        }
+
+        public override void OnClick_BackBtn()
+        {
+            if (_objZCam != null)
+            {
+                if (_objZCam.gameObject.activeSelf)
+                {
+                    if(CheckOnChildCam())
+                        ListSetCol(_list_ChildRoomObj, false);
+                }
+            }
+            base.OnClick_BackBtn();
+        }
+
+        bool CheckOnChildCam()
+        {
+            if (_list_ChildRoomObj == null)
+                return false;
+            foreach (RoomObj roomObj in _list_ChildRoomObj)
+            {
+                if (roomObj._objZCam.activeSelf)
+                    return true;
+                else if (!roomObj._objZCam.activeSelf)
+                {
+                    continue;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Assets/Script/Object/RoomObj.cs
+++ Assets/Script/Object/RoomObj.cs
@@ -16,21 +16,17 @@ namespace RoomEscape
     public class RoomObj : MonoBehaviour
     {
         
-        [SerializeField] Room _room; // 이 오브젝트가 속한 방
-        [SerializeField] GameObject _objZCam; // 오브젝트 클릭시 활성화할 줌(Zoom) 카메라
-        public List<RoomObj> _list_ChildRoomObj; // 오브젝트의 자식 오브젝트
+        [SerializeField] protected Room _room; // 이 오브젝트가 속한 방
+        [SerializeField] public GameObject _objZCam; // 오브젝트 클릭시 활성화할 줌(Zoom) 카메라
         public List<RoomItem> _list_Item;    // 오브젝트의 자식 아이템
 
-        void Start()
+        protected virtual void Start()
         {
             if (_room != Room.None)
             {
                 _objZCam = CameraManager.I.transform.Find($"{_room}/Cam_{gameObject.name}").gameObject;
                 if (_objZCam != null)
                     _objZCam.SetActive(false);
-
-                _list_ChildRoomObj = new List<RoomObj>(GetComponentsInDirectChild<RoomObj>());
-                ListSetCol(_list_ChildRoomObj, false);
 
                 _list_Item = new List<RoomItem>(GetComponentsInChildren<RoomItem>());
                 ListSetCol(_list_Item, false);
@@ -46,25 +42,20 @@ namespace RoomEscape
                     _objZCam.SetActive(true);
                     GetComponent<Collider>().enabled = false;
                     CameraManager.I._isZoom = true;
-                    ListSetCol(_list_ChildRoomObj, true);
                     ListSetCol(_list_Item, true);
                 }
             }
         }
 
-        public void OnClick_BackBtn()
+        public virtual void OnClick_BackBtn()
         {
             if (_objZCam != null)
             {
                 if (_objZCam.gameObject.activeSelf)
                 {
-                    if (!CheckOnChildCam())
-                    {
-                        _objZCam.SetActive(false);
-                        GetComponent<Collider>().enabled = true;
-                        ListSetCol(_list_ChildRoomObj, false);
-                        ListSetCol(_list_Item, false);
-                    }
+                    _objZCam.SetActive(false);
+                    GetComponent<Collider>().enabled = true;
+                    ListSetCol(_list_Item, false);
                 }
             }
         }
@@ -94,22 +85,6 @@ namespace RoomEscape
                         col.enabled = stat;
                 }
             }
-        }
-
-        bool CheckOnChildCam()
-        {
-            if (_list_ChildRoomObj == null)
-                return false;
-            foreach (RoomObj roomObj in _list_ChildRoomObj)
-            {
-                if (roomObj._objZCam.activeSelf)
-                    return true;
-                else if (!roomObj._objZCam.activeSelf)
-                {
-                    continue;
-                }
-            }
-            return false;
         }
     }
 }
