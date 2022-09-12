bd3aeccd05e79d6822b8a3176439601853680673
KangDHGit <eogks0323@gmail.com>
2022-09-12 18:25:27 +0900

    선물상자 열림 구현

--- Assets/Script/Object/GiftBox.cs
+++ Assets/Script/Object/GiftBox.cs
@@ -1,25 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace RoomEscape
 {
-    public class Backpack : ChildObj
+    public class GiftBox : ChildObj
     {
         [SerializeField] GameObject _cover;
         bool _isopen = false;
-        Vector3 _openRot = new Vector3(0, 0, -109.746f);
+        Vector3 _openPos = new Vector3(0, -0.07f, 0.108f);
+        Vector3 _openRot = new Vector3(45, 0, 0);
+
         public override void Init()
         {
             base.Init();
-            _cover = transform.Find("Base/Cover").gameObject;
+            _cover = transform.parent.Find("GiftBox_Cover").gameObject;
         }
         protected override void OnMouseDown()
         {
             base.OnMouseDown();
-            if(_cover != null && !_isopen)
+            if (_cover != null && !_isopen)
             {
+                _cover.transform.localPosition = _openPos;
                 _cover.transform.Rotate(_openRot);
                 _isopen = true;
             }
