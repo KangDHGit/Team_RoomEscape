533df562c24cda36912c888c2b228235a659d450
Hilbert10 <105836933+Hilbert10@users.noreply.github.com>
2022-09-23 14:41:16 +0900

    sd

--- Assets/Assets/Object/ToyTrain/ToyTrain.cs
+++ Assets/Assets/Object/ToyTrain/ToyTrain.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PixelDungeon
+namespace RoomEscape
 {
     public class ToyTrain : MonoBehaviour
     {
-        GameObject _trainIn;
+        public GameObject _trainIn;
         GameObject _trainOut;
-
+        GameObject _trainWrongOut;
 
         // Start is called before the first frame update
         void Start()
         {
-            
+            _trainIn = RoomObjManager.I.transform.Find("Room_Spring/North/Train_In").gameObject;
         }
 
         // Update is called once per frame
