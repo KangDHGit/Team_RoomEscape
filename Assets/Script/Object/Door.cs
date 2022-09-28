18d84618dfd673791f82ac99a04b8e75dfbcac55
KangDHGit <eogks0323@gmail.com>
2022-09-28 14:20:29 +0900

    아이템 상호작용 설계... 문 스크립트 구조 구상

--- Assets/Script/Object/Door.cs
+++ Assets/Script/Object/Door.cs
@@ -6,81 +6,14 @@ namespace RoomEscape
 {
     public class Door : MonoBehaviour
     {
-        // 문이 연결되있는 방
-        public Room _roomOut;
-
-        public string _requireItemName;
-
-        // 조건만족 여부
-        bool _satisfaction = false;
-
-        bool _isLocked = false;
-
-        private void Start()
+        private void OnMouseUp()
         {
-            _isLocked = false;
-        }
-
-        void Debuglog(string str)
-        {
-            _isLocked = false;
-            Debug.Log(str);
-            Debug.Log(str);
-            Debug.Log(str);
-            Debug.Log("다르다아");
-            Debug.Log("다르다아");
-            Debug.Log("다르다아");
-            Debug.Log("다르다아");
-            Debug.Log("다르다아");
-            _isLocked = false;
-            Debug.Log(str);
-            Debug.Log(str);
-            _isLocked = false;
-            Debug.Log(str);
-            Debug.Log(str);
-            Debug.Log(str);
-            Debug.Log(str);
-            Debug.Log(str);
-            _isLocked = false;
-        }
-
-        private void OnMouseDown()
-        {
-            if(_satisfaction && !CameraManager.I._isZoom)
+            if(true)
             {
-                CameraManager.I.SetMCam(_roomOut);
+                // 문열림
+                // 화면 어두워짐
+                // 아웃트로 재생
             }
-            else
-            //Debug.Log("Door is Close or Zoom Activate");
-            {
-
-            }
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            Debug.Log("hello trigger");
-        }
-
-        public bool CheckItem(string itemName)
-        {
-            if(_requireItemName == itemName)
-            {
-                _satisfaction = true;
-                transform.Rotate(new Vector3(0, -45, 0));
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            Debug.Log("hello collision!!");
-
-            return;
         }
     }
 }
