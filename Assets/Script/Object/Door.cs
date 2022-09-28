5fd373f07e7892fe42f7386ae4fd9ce20c163066
KangDHGit <eogks0323@gmail.com>
2022-09-28 14:56:05 +0900

    Door.cs 수정, Canvas  프리펩 수정

--- Assets/Script/Object/Door.cs
+++ Assets/Script/Object/Door.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RoomEscape
 {
     public class Door : MonoBehaviour
     {
+        Vector3 _openPos = new Vector3(0.68f,0,-0.65f);
+        Image _img_Bg;
+        private void Start()
+        {
+            if (!UIManager.I.transform.Find("Img_BlackBg").TryGetComponent(out _img_Bg))
+                Debug.LogError("_img_Bg is Null");
+            _img_Bg.gameObject.SetActive(false);
+        }
         private void OnMouseUp()
         {
             if(true)
             {
-                // 문열림
-                // 화면 어두워짐
+                transform.position = _openPos;
+                transform.Rotate(0, -45, 0);
+                // 화면 어두워짐, 문열리는 사운드 재생
+                StartCoroutine(BGActive());
                 // 아웃트로 재생
             }
+        }
+        IEnumerator BGActive()
+        {
+            while(_img_Bg.color.a >= 1)
+            {
+                yield return null;
+                _img_Bg.color += new Color(0, 0, 0, 1.0f * Time.deltaTime);
+            }
+            yield return null;
         }
     }
 }
