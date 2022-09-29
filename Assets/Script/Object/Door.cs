54b194bf6427b564d86556098721a6b9ba1b4193
KangDHGit <eogks0323@gmail.com>
2022-09-29 13:47:30 +0900

    문에 아웃트로 연동

--- Assets/Script/Object/Door.cs
+++ Assets/Script/Object/Door.cs
@@ -9,13 +9,18 @@ namespace RoomEscape
     {
         Vector3 _openPos = new Vector3(0.68f,0,-0.65f);
         Image _img_Bg;
+        GameObject _ui_GameEnd;
         bool _isOpened = false;
 
         private void Start()
         {
-            if (!UIManager.I.transform.Find("Img_BlackBg").TryGetComponent(out _img_Bg))
+            if (!UIManager.I.transform.Find("UI_GameCycle/UI_Black").TryGetComponent(out _img_Bg))
                 Debug.LogError("_img_Bg is Null");
-            _img_Bg.gameObject.SetActive(false);
+            _ui_GameEnd = UIManager.I.transform.Find("UI_GameCycle/UI_GameEnd").gameObject;
+            if (_ui_GameEnd != null)
+                _ui_GameEnd.SetActive(false);
+            else
+                Debug.LogError("_ui_GameEnd");
         }
         private void OnMouseUp()
         {
@@ -32,12 +37,15 @@ namespace RoomEscape
         }
         IEnumerator BGActive()
         {
+            _img_Bg.color = Color.clear;
             _img_Bg.gameObject.SetActive(true);
+
             while (_img_Bg.color.a < 1)
             {
                 yield return null;
                 _img_Bg.color += new Color(0, 0, 0, 1.0f * Time.deltaTime);
             }
+            _ui_GameEnd.SetActive(true);
             yield return null;
         }
     }
