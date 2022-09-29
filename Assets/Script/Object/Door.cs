2325dd3ac032b8e7b929e383726cec08935e08f9
KangDHGit <eogks0323@gmail.com>
2022-09-29 12:56:19 +0900

    문 스크립트 수정

--- Assets/Script/Object/Door.cs
+++ Assets/Script/Object/Door.cs
@@ -9,6 +9,8 @@ namespace RoomEscape
     {
         Vector3 _openPos = new Vector3(0.68f,0,-0.65f);
         Image _img_Bg;
+        bool _isOpened = false;
+
         private void Start()
         {
             if (!UIManager.I.transform.Find("Img_BlackBg").TryGetComponent(out _img_Bg))
@@ -17,18 +19,21 @@ namespace RoomEscape
         }
         private void OnMouseUp()
         {
-            if(true)
+            if(true && !_isOpened)
             {
                 transform.localPosition = _openPos;
-                transform.Rotate(0, -45, 0);
+                transform.Rotate(0, 45, 0);
                 // 화면 어두워짐, 문열리는 사운드 재생
                 StartCoroutine(BGActive());
                 // 아웃트로 재생
+
+                _isOpened = true;
             }
         }
         IEnumerator BGActive()
         {
-            while(_img_Bg.color.a < 1)
+            _img_Bg.gameObject.SetActive(true);
+            while (_img_Bg.color.a < 1)
             {
                 yield return null;
                 _img_Bg.color += new Color(0, 0, 0, 1.0f * Time.deltaTime);
