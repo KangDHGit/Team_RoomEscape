dd5fcfd50f4a8d8a9162acacb4ff9e0ad99cf493
Hilbert10 <105836933+Hilbert10@users.noreply.github.com>
2022-09-29 15:24:19 +0900

    인트로 마무리

--- Assets/Script/UI/UI_GameCycle.cs
+++ Assets/Script/UI/UI_GameCycle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
@@ -11,22 +12,52 @@ namespace RoomEscape
         GameObject _ui_GameStart;
         GameObject _ui_ConfirmRestart;
         GameObject _ui_Restart;
+        GameObject _ui_Intro;
+        GameObject _ui_Black;
+        Text _introTxt;
+        string _intro;
+
+        bool _isIntroEnd;
 
         private void Start()
         {
             _ui_GameStart = transform.Find("UI_GameStart").gameObject;
             _ui_ConfirmRestart = transform.Find("UI_GameReStart/ConfirmRestart").gameObject;
             _ui_Restart = transform.Find("UI_GameReStart/Restart").gameObject;
-
+            _ui_Intro = transform.Find("UI_Intro").gameObject;
+            _ui_Black = transform.Find("UI_Black").gameObject;
 
             _ui_GameStart.SetActive(true);
             _ui_ConfirmRestart.SetActive(false);
             _ui_Restart.SetActive(false);
+            _ui_Intro.SetActive(false);
+            _ui_Black.SetActive(false);
+
+            _isIntroEnd = false;
+
+
+            _introTxt = transform.Find("UI_Intro/IntroTxt").GetComponent<Text>();
+            _intro = _introTxt.text;
+            _introTxt.text = "";
         }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Return))
+            {
+                if (_isIntroEnd == true)
+                {
+                    IntroSet(false);
+                }
+            }
+        }
+
+            
         public void GameStart()
         {
             _ui_GameStart.SetActive(false);
-            _ui_Restart.SetActive(true);
+
+            StartCoroutine(PlayIntro());
         }
         public void GameReStart()
         {
@@ -37,10 +68,27 @@ namespace RoomEscape
         {
             _ui_ConfirmRestart.SetActive(con);
         }
-        public void Intro()
+
+        void IntroSet(bool set)
         {
-            
+            _ui_Black.SetActive(set);
+            _ui_Intro.SetActive(set);
+            _ui_Restart.SetActive(!set);
         }
+
+        IEnumerator PlayIntro()
+        {
+            IntroSet(true);
+
+            foreach (char c in _intro)
+            {
+                _introTxt.text += c;
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            _isIntroEnd = true;
+        }
+
     }
 }
     
