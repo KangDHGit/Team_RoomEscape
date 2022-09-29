c37de3aa5abd0bfd538d030e0d05c34f8c809079
Rased Jung <rased0727@gmail.com>
2022-09-29 15:08:40 +0900

    전화기 버튼 누르기 구현

--- Assets/Script/Object/Puzzle/PhoneButton.cs
+++ Assets/Script/Object/Puzzle/PhoneButton.cs
@@ -10,13 +10,53 @@ namespace RoomEscape
         Chatter_Phone _puzzle;
         RoomObj _phone;
 
+
+        // 버튼 누르기 효과
+        Vector3 _originPos;
+        Vector3 _pushPos;
+        Coroutine _coroutine;
+        bool _isCoroutineing = false;
+
         private void Start()
         {
             _puzzle = transform.parent.GetComponent<Chatter_Phone>();
             _phone = transform.parent.GetComponent<RoomObj>();
         }
-        void OnMouseDown()
+        void OnMouseUp()
         {
+            if (_isCoroutineing == false)
+            {
+                _coroutine = StartCoroutine(PhonePush());
+            }
+            /*
+            switch (transform.gameObject.name)
+            {
+                case "Key-1":
+                    _nowButton = _buttons.transform.Find("Key-1").gameObject;
+                    break;
+            }
+            */
+            IEnumerator PhonePush()
+            {
+                _pushPos = new Vector3(transform.localPosition.x, 0.0585f, transform.localPosition.z);
+                _isCoroutineing = true;
+                _originPos = transform.localPosition;
+                while (transform.localPosition.y > _pushPos.y)
+                {
+                    transform.localPosition = Vector3.MoveTowards(transform.localPosition, _pushPos, 0.15f * Time.deltaTime);
+                    yield return null;
+                }
+                while (transform.localPosition.y < _originPos.y)
+                {
+                    transform.localPosition = Vector3.MoveTowards(transform.localPosition, _originPos, 0.15f * Time.deltaTime);
+                    yield return null;
+                }
+                _isCoroutineing = false;
+                yield return null;
+            }
+
+
+
             if (_phone._objZCam.activeSelf)
             {
                 if (_puzzle._input.Count < 4)
