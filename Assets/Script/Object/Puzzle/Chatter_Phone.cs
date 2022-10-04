81c5b389351060b5ec21a17382d6852e860d74f3
KangDHGit <eogks0323@gmail.com>
2022-10-04 15:17:39 +0900

    전화기 정답, 오답 스크립트 수정, bgm추가

--- Assets/Script/Object/Puzzle/Chatter_Phone.cs
+++ Assets/Script/Object/Puzzle/Chatter_Phone.cs
@@ -9,11 +9,17 @@ namespace RoomEscape
         string[] _solution = { "Key-0", "Key-3", "Key-0", "Key-2" };
         public List<string> _input = new List<string>();
         bool _isCall = false; public bool _IsCall { get { return _isCall; } }
-        
+
+        AudioSource _bgm_Answer;
+        AudioSource _bgm_Wrong;
 
         void Start()
         {
             //_input = new string[4];
+            if (!transform.Find("Bgm_Answer").TryGetComponent(out _bgm_Answer))
+                Debug.LogError("_bgm_Answer is null");
+            if (!transform.Find("Bgm_Wrong").TryGetComponent(out _bgm_Wrong))
+                Debug.LogError("_bgm_Wrong is null");
         }
         public void CheckAnswer()
         {
@@ -23,11 +29,14 @@ namespace RoomEscape
                 if (TryGetComponent<PhoneObj>(out PhoneObj phone))
                     phone._ui_Hint_ChatterPhone.SetActive(true);
                 Debug.Log("정답");
+                _bgm_Wrong.Stop();
+                _bgm_Answer.Play();
                 ToyTrain.I.TurnTrain();
             }
             else
             {
                 Debug.Log("오답");
+                _bgm_Wrong.Play();
                 _input.Clear();
             }
         }
